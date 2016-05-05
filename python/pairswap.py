5f9cdf2b6d9753901a9b707a83f0ccbfd6c1bb20
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-05-04 23:47:59 -0700

    comments of intended

--- python/pairswap.py
+++ python/pairswap.py
@@ -1,6 +1,11 @@
 def pair_swap(a):
+    """
+    Swap odd-even pairs of adjacent bits
+    :param a: The binary input in the form A_n, ...A3, A2 A1, A0
+    :return: Binary of the form A_n-1, A_n, ... A2, A3, A0, A1
+    """
     swapped = 0
-    
+
     while a > 0:
         print bin(swapped)
         if (a & 0b11 == 1) or (a & 0b11 == 2):
