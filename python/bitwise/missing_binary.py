19364ca63cd31fa5d138fc29983339f41a2717b4
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-05-08 21:30:57 -0700

    Added some basic test cases

--- python/bitwise/missing_binary.py
+++ python/bitwise/missing_binary.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def get_bit(binary, bit_index):
     """Retrieve the bit at the specified index of a given binary number
 
@@ -9,11 +12,11 @@ def get_bit(binary, bit_index):
 
 
 def missing_binary(bin_sequence):
-    """Given an ordered list of binary numbers whose values range from 0 to N, there is
-    an element that is missing, find that value within linear time by
+    """Given an ordered list of binary numbers whose values range from 0 to N (inclusive), there is
+    an element that is missing, find that value within linear time.
 
-    :param bin_sequence: An ordered sequence of integers from 0 to N with one missing number
-    :return: The missing number of the sequence
+    :param bin_sequence: An ordered sequence of integers from 0 to N (inclusive) with one missing number
+    :return: The missing number n of the sequence. 0 <= n < N
     """
     return _missing_binary(bin_sequence, 0)
 
@@ -37,3 +40,14 @@ def _missing_binary(bin_sequence, current_index):
         #There's more 0 bits, indicating that the missing binary has LSB of 1
         #so we'll continue the search with all binaries with LSB of 1
         return (_missing_binary(lsb_of_ones, current_index+1) << 1) | 1
+
+
+class TestMissingBinary(unittest.TestCase):
+
+    def test_normal(self):
+        self.assertEqual(missing_binary([0, 1, 2, 4]), 0b11)
+        self.assertEqual(missing_binary([1, 2, 3]), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
