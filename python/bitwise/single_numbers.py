dac40c13667a1c56715ea6e691f37d72c6f70ca2
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-10-11 23:05:53 -0700

    Add problem explaination for single_numbers

--- python/bitwise/single_numbers.py
+++ python/bitwise/single_numbers.py
@@ -1,8 +1,17 @@
 def single_numbers(numbers):
+    """
+    Given a array of numbers, where exactly two numbers appear only once (a, b)
+    and all other elements appear twice, find a and b.
+
+    :param numbers: Array of positive non-zero number which contains a duplicate for all elements except two.
+    :return: The two exceptional numbers which didn't appear twice.
+    """
     xor_result = 0
     for num in numbers:
         xor_result ^= num
 
+    # We're interested in a bit position with value 1 of the xor_result
+    # it tells which bit to compare all other number against to find either a or b
     interesting_bit = 1
     while xor_result & interesting_bit == 0:
         interesting_bit <<= 1
@@ -15,6 +24,7 @@ def single_numbers(numbers):
         else:
             second_single ^= num
 
-    return (first_single, second_single)
+    return first_single, second_single
+
 
 print single_numbers([1,2,1,3,2,5])
