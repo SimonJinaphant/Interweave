f17fae9795d6a1096469f769a5084b26071c433c
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-10-14 22:58:34 -0700

    Add Kth smallest element in matrix

---
+++ python/ksmallestMat.py
@@ -0,0 +1,44 @@
+from math import sqrt, pow
+
+
+def k_smallest_matrix(matrix, k):
+    """
+    Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+    find the kth smallest element in the matrix.
+
+    :param matrix: A square matrix of size n x n in sorted order for its rows and cols
+    :param k: The kth-smallest element to look for
+
+    :return: An k-th smallest element in the matrix.
+    """
+    sqrt_k = sqrt(k)
+    cut_off = int(sqrt_k)
+
+    if sqrt_k == float(cut_off):
+        # K is a perfect square so we'll return the diagonal
+        return matrix[cut_off-1][cut_off-1]
+
+    # The answer is within the subvectors
+    cut_off_row = 0
+    cut_off_col = 0
+    current_number = None;
+
+    # Find how many elements we need to check until we reach the new k value
+    until_k = k - int(pow(cut_off, 2.0))
+
+    while until_k != 0:
+        if matrix[cut_off][cut_off_col] < matrix[cut_off_row][cut_off]:
+            current_number = matrix[cut_off][cut_off_col]
+            cut_off_col += 1
+        else:
+            current_number = matrix[cut_off_row][cut_off]
+            cut_off_row += 1
+
+        until_k -= 1
+
+    return current_number
+
+print k_smallest_matrix([[1, 5, 9],
+                         [10, 11, 13],
+                         [12, 13, 15]],
+                        8)
