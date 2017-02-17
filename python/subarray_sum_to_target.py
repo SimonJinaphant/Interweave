54e03778205189d9c8a097cfc2c40908914c80a8
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2017-02-16 18:22:56 -0800

    Add testcase to subarray_sum_to_target

--- python/subarray_sum_to_target.py
+++ python/subarray_sum_to_target.py
@@ -1,20 +1,34 @@
+from __future__ import absolute_import
+import unittest
+
+
 def subarray_sum_to_target(numbers, target):
     start = 0
     end = 1
-    sum = numbers[start]
+    current_sum = numbers[start]
 
     while end != len(numbers):
-        if sum < target:
-            sum += numbers[end]
+        if current_sum < target:
+            current_sum += numbers[end]
             end += 1
-        elif sum > target:
-            sum -= numbers[start]
+        elif current_sum > target:
+            current_sum -= numbers[start]
             start += 1
         else:
             return numbers[start: end]
 
     return None
 
-print subarray_sum_to_target([1,4,20,3,10,5], 33)
-print subarray_sum_to_target([1,4,0,0,3,10,5], 7)
-print subarray_sum_to_target([1,4], 0)
+
+class SubarraySumToTargetTest(unittest.TestCase):
+
+    def test_valid(self):
+        self.assertEquals(subarray_sum_to_target([1, 4, 20, 3, 10, 5], 33), [20, 3, 10])
+        self.assertEquals(subarray_sum_to_target([1, 4, 0, 0, 3, 10, 5], 7), [4, 0, 0, 3])
+        self.assertEqual(subarray_sum_to_target([1, 3, 10, 5, 1, 6], 16), [10, 5, 1])
+
+    def test_invalid(self):
+        self.assertEquals(subarray_sum_to_target([1, 4], 0), [])
+
+if __name__ == "__main__":
+    unittest.main()
